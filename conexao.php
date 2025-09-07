ee61a4e6f704943f1fdf08259c30f74a5537a227
Francisco Damião Terto <contato@damiaoterto.dev>
2025-09-07 11:38:14 -0300

    refactor: remove production errors

--- conexao.php
+++ conexao.php
@@ -8,6 +8,10 @@ if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 
+header('Cache-Control: no-store, no-cache, must-revalidate, max-age=0');
+header('Cache-Control: post-check=0, pre-check=0', false);
+header('Pragma: no-cache');
+
 if (file_exists($envPath)) {
     $dotenv = \Dotenv\Dotenv::createImmutable(__DIR__);
     $dotenv->load();
